a51f2cf627eb3399417a32d3926b277eb5834c66
Kenneth Fernandes <kennethdominic57@gmail.com>
2024-07-09 22:16:39 +0530

    added main.py and code for multithreading

--- audio_processing.py
+++ audio_processing.py
@@ -2,16 +2,25 @@ import speech_recognition as sr
 import pyttsx3
 
 # Initialize the recognizer and text-to-speech engine
-r: sr.Recognizer = sr.Recognizer()
-engine: pyttsx3.engine.Engine = pyttsx3.init()
+r: sr.Recognizer = None
+engine: pyttsx3.engine.Engine = None
+
+def audio_load():
+    global r, engine
+    r = sr.Recognizer()
+    engine = pyttsx3.init()
 
 # TODO: Add Documentation
 def SpeakText(command: str):
+    if(engine == None):
+        raise Exception("Text-to-speech engine not loaded, call audio_load() first.")
     engine.say(command)
     engine.runAndWait()
 
 # TODO: Add Documentation
 def SpeechToText() -> str:
+    if(r == None):
+        raise Exception("Recognizer not loaded, call audio_load() first.")
     try:
         with sr.Microphone() as source:
             print("Listening...")
