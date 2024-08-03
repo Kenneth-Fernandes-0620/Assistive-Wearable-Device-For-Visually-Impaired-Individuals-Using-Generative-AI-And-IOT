f29676396f7842b2c4ba46ed0e0559a8de04da78
Kenneth Fernandes <kennethdominic57@gmail.com>
2024-08-03 17:23:14 +0530

    added code for camera check and updated requirements.txt

--- client/image_processing.py
+++ client/image_processing.py
@@ -12,10 +12,24 @@ def load_image_capture():
 # TODO: Add Documentation
 def get_image_from_webcam():
     global video_capture
-    ret, frame = video_capture.read()    
+    ret, frame = video_capture.read()
     if not ret:
         raise Exception("Unable to capture image from camera")
     return cv2.imencode(".jpg", frame, [cv2.IMWRITE_JPEG_QUALITY, 90])
 
+
 def free_video_capture():
     video_capture.release()
+
+
+def list_cameras():
+    camera_index = 0
+    cap = cv2.VideoCapture(camera_index)
+    if cap.read()[0]:
+        print(f"Camera {camera_index} is available")
+    else:
+        print(f"Camera {camera_index} is not available")
+    cap.release()
+
+if __name__ == "__main__":
+    list_cameras()
